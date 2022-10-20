94f0b173395a600f26a30da18174a916ad55e7cf
Johann Cubillos <jacubillosg@mail.ru>
2022-10-19 20:44:38 -0500

    Added graphics to draw in the future

--- FinalProject/app/src/main/java/co/edu/unal/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/co/edu/unal/finalproject/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
@@ -12,6 +16,7 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private LineGraphSeries<DataPoint> series;
     private matlabLexer lexer = new matlabLexer(CharStreams.fromString("2.0;"));
     private CommonTokenStream tokens = new CommonTokenStream(lexer);
     private matlabParser parser = new matlabParser(tokens);
@@ -23,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         interpreter.visit(tree);
+
+        double x_axis = 0.0;
+        double y_axis;
+        int numDataPoints = 100;
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<>();
+        for(int i = 0; i < numDataPoints; i++) {
+            x_axis += 0.1;
+            y_axis = Math.sin(x_axis);
+            series.appendData(new DataPoint(x_axis, y_axis), true, 100);
+        }
+        graph.addSeries(series);
     }
 }
