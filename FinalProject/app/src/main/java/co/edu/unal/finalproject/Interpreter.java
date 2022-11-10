6a95a4a4eea44992d11034d57f94c72c4daabb51
Juan Reina <jreinaz@unal.edu.co>
2022-11-10 18:21:07 -0500

    Input archivo de texto en carpeta raw en res

--- FinalProject/app/src/main/java/co/edu/unal/finalproject/Interpreter.java
+++ FinalProject/app/src/main/java/co/edu/unal/finalproject/Interpreter.java
@@ -2,10 +2,27 @@ package co.edu.unal.finalproject;
 
 import android.util.Log;
 
+import java.util.HashMap;
+
 public class Interpreter<T> extends matlabBaseVisitor<T>{
+    HashMap<String, Object> simTable = new HashMap<String, Object>();
     @Override
     public T visitPrimary_expression(matlabParser.Primary_expressionContext ctx) {
-        Log.i("primary_expression: ",ctx.getText());
-        return super.visitPrimary_expression(ctx);
+        //Log.i("primary_expression: ",ctx.getText());
+        if(ctx.IDENTIFIER() != null){
+            if(ctx.IDENTIFIER().getText() == "true"){
+                Boolean result = ctx.IDENTIFIER().getText() =="true";
+                return (T) result;
+            }else if(ctx.IDENTIFIER().getText() == "false"){
+                Boolean result = !(ctx.IDENTIFIER().getText() == "false");
+                return (T) result;
+            }
+        }else if(ctx.CONSTANT() != null){
+            Double num = new Double(ctx.CONSTANT().getText());
+            return (T) num;
+        }else if(ctx.expression() != null){
+            return this.visitExpression(ctx.expression());
+        }
+        return null;
     }
 }
--- FinalProject/app/src/main/java/co/edu/unal/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/co/edu/unal/finalproject/MainActivity.java
@@ -3,25 +3,35 @@ package co.edu.unal.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private matlabLexer lexer = new matlabLexer(CharStreams.fromString("2.0;"));
-    private CommonTokenStream tokens = new CommonTokenStream(lexer);
-    private matlabParser parser = new matlabParser(tokens);
-    private ParseTree tree = parser.translation_unit();
-    private Interpreter interpreter = new Interpreter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        matlabLexer lexer = null;
+        try {
+            lexer = new matlabLexer(CharStreams.fromStream(getResources().openRawResource(R.raw.input)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        matlabParser parser = new matlabParser(tokens);
+        ParseTree tree = parser.translation_unit();
+        Interpreter interpreter = new Interpreter();
         interpreter.visit(tree);
     }
 }
