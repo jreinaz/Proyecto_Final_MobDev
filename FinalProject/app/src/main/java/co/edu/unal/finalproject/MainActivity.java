8f5615b5762ab6214781b0484ce76ffbfd623715
Juan Reina <jreinaz@unal.edu.co>
2022-11-10 18:21:07 -0500

    Input archivo de texto en carpeta raw en res

--- FinalProject/app/src/main/java/co/edu/unal/finalproject/MainActivity.java
+++ FinalProject/app/src/main/java/co/edu/unal/finalproject/MainActivity.java
@@ -3,6 +3,7 @@ package co.edu.unal.finalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 
 import com.jjoe64.graphview.GraphView;
@@ -13,7 +14,11 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private LineGraphSeries<DataPoint> series;
@@ -27,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        matlabLexer lexer = null;
+        try {
+            lexer = new matlabLexer(CharStreams.fromStream(getResources().openRawResource(R.raw.input)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        matlabParser parser = new matlabParser(tokens);
+        ParseTree tree = parser.translation_unit();
+        Interpreter interpreter = new Interpreter();
         interpreter.visit(tree);
 
         double x_axis = 0.0;
